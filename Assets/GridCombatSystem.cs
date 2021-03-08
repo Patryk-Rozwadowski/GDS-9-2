f40ab9a884b4a9a2615765db9cac44e162c61092
Patryk Rozwadowski <pm.rozwadowski@gmail.com>
2021-03-08 23:23:55 +0100

    feat: split units into teams

---
+++ Assets/GridCombatSystem.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class GridCombatSystem : MonoBehaviour {
+    [SerializeField] private UnitCombatSystem[] unitCombatSystemsArray;
+    
+    private UnitCombatSystem _unitCombatSystem;
+    private List<UnitCombatSystem> _leftTeam, _rightTeam;
+    
+    void Start() {
+        _leftTeam = new List<UnitCombatSystem>();
+        _rightTeam = new List<UnitCombatSystem>();
+
+        foreach (var unit in unitCombatSystemsArray) {
+            CombatSystemUnitDebugLogger(unit);
+
+            if (unit.GetTeam() == Team.Left) {
+                _leftTeam.Add(unit);
+            }
+            else {
+                _rightTeam.Add(unit);
+            }
+        }
+    }
+    // Update is called once per frame
+    void Update() {
+    }
+
+    private void CombatSystemUnitDebugLogger(UnitCombatSystem unit) {
+        Debug.Log($"Unit name: {unit.name}, team: {unit.GetTeam()}");
+    }
+}
---
+++ Assets/Scripts/UnitCombatSystem.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public enum Team {
+    Left,
+    Right
+}
+
+public class UnitCombatSystem : MonoBehaviour {
+    [SerializeField] private Team team;
+
+    // Update is called once per frame
+    void Update() {
+    }
+
+    public Team GetTeam() {
+        return team;
+    }
+}
