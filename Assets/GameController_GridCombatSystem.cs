9f72408674c44eb44ec29b4bd6cd9fa31be6693c
Patryk Rozwadowski <pm.rozwadowski@gmail.com>
2021-03-09 22:26:36 +0100

    refactor: add state and moveTo

---
+++ Assets/GameController_GridCombatSystem.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController_GridCombatSystem : MonoBehaviour {
+    public static GameController_GridCombatSystem Instance { get; private set; }
+
+    private Grid<GridCombatSystem.GridObject> _grid;
+    private GridPathfinding _gridPathfinding;
+    
+    void Start() {
+        Instance = this;
+        
+        int mapWidth = 10;
+        int mapHeight = 8;
+        float cellSize = 10f;
+        Vector3 origin = new Vector3(0, 0);
+
+        _grid = new Grid<GridCombatSystem.GridObject>(mapWidth, mapHeight, cellSize, origin, (Grid<GridCombatSystem.GridObject> g, int x, int y) => new GridCombatSystem.GridObject(g, x, y));
+
+        _gridPathfinding = new GridPathfinding(origin + new Vector3(1, 1) * cellSize * .5f, new Vector3(mapWidth, mapHeight) * cellSize, cellSize);
+        // _gridPathfinding.RaycastWalkable();
+        // gridPathfinding.PrintMap((Vector3 vec, Vector3 size, Color color) => World_Sprite.Create(vec, size, color));
+
+    }
+
+    public Grid<GridCombatSystem.GridObject> GetGrid() {
+        return _grid;
+    }
+}
--- Assets/Scripts/UnitCombatSystem.cs
+++ Assets/Scripts/UnitCombatSystem.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public enum Team {
@@ -9,11 +10,42 @@ public enum Team {
 public class UnitCombatSystem : MonoBehaviour {
     [SerializeField] private Team team;
 
-    // Update is called once per frame
-    void Update() {
+    private MovePositionPathfinding _movePositionPathfinding;
+    private State _state;
+
+    private enum State {
+        Normal,
+        Moving,
+        Attacking
+    }
+
+    private void Start() {
+        _movePositionPathfinding = GetComponent<MovePositionPathfinding>();
+        _state = State.Normal
+    }
+
+    private void Update() {
+        switch (_state) {
+            case State.Normal:
+                break;
+            case State.Moving:
+                break;
+            case State.Attacking:
+                break;
+        }
     }
 
     public Team GetTeam() {
         return team;
+    }
+
+    public void MoveTo(Vector3 targetPosition, Action onReachedPosition) {
+        _state = State.Moving;
+
+        // PATHFINDING
+        _movePositionPathfinding.SetMovePosition(targetPosition + new Vector3(1, 1), () => {
+            _state = State.Normal;
+            onReachedPosition();
+        });
     }
 }
