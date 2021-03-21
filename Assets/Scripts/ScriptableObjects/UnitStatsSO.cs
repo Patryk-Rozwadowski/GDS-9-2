6f366958d924c961a0c280cdf4998a0fc03b6dfc
Patryk Rozwadowski <pm.rozwadowski@gmail.com>
2021-03-21 21:44:38 +0100

    feat: add scriptable object for unit creator

---
+++ Assets/Scripts/ScriptableObjects/UnitStatsSO.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Units/Creator")]
+public class UnitStatsSO : ScriptableObject {
+    [Header("Maximum unit's health.")] [SerializeField]
+    private int maxHealth;
+
+    [Header("Unit's damage")] [SerializeField]
+    private int damage;
+
+    [Header("Unit's sprite.")] [SerializeField]
+    private Sprite sprite;
+
+    [Header("Maximum movement range")] [SerializeField]
+    private int movementRange;
+
+    [Header("Maximum attack range")] [SerializeField]
+    private int attackRange;
+}
