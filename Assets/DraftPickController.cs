6a6c7b39a278c855e89a6058df88235a16686438
Patryk Rozwadowski <pm.rozwadowski@gmail.com>
2021-03-24 23:55:57 +0100

    refactor: better synchronization draft - grid, team panels are active depending on picking team

--- Assets/DraftPickController.cs
+++ Assets/DraftPickController.cs
@@ -6,7 +6,6 @@ using UnityEngine.UI;
 
 public class DraftPickController : MonoBehaviour {
     [SerializeField] private GameObject _leftTeamPanel, _rightTeamPanel;
-    public List<GameObject> _leftTeam, _rightTeam;
 
     private enum Team {
         Left,
@@ -16,10 +15,8 @@ public class DraftPickController : MonoBehaviour {
     private bool
         _pickedPosition,
         _leftPickedUnit,
-        _rightPickedUnit,
         _rightPicked,
-        _leftPutUnitOnMap,
-        _rightPutUnitOnMap;
+        _leftPutUnitOnMap;
 
     private int _numberOfUnitsInTeams;
     private GameObject _pickedUnit;
@@ -28,57 +25,46 @@ public class DraftPickController : MonoBehaviour {
 
     private void Awake() {
         _gridCombatSystem = GameObject.Find("GridCombatSystem").GetComponentInChildren<GridCombatSystem>();
-
     }
 
     void Start() {
-        // _rightTeamPanel.SetActive(false);
-        _leftTeam = new List<GameObject>();
-        _rightTeam = new List<GameObject>();
-
+        
         _teamPicking = Team.Left;
 
         _leftPickedUnit = false;
-        _rightPickedUnit = false;
-
-        _leftPutUnitOnMap = false;
-        _rightPutUnitOnMap = false;
-
         _pickedUnit = null;
         _numberOfUnitsInTeams = 5;
-
+        _rightTeamPanel.SetActive(false);
     }
 
     public void PickUnit(GameObject element) {
         var elementCombatSystem = element.GetComponent<UnitCombatSystem>();
         var pickedElement = elementCombatSystem.GetTeam();
-        
+
         Debug.Log($"PICKED ELEMENT TEAM: {pickedElement}");
         Debug.Log($"TEAM PICKING: {_teamPicking}");
         _pickedPosition = false;
-        
+
         if ((Team) pickedElement == _teamPicking && _teamPicking == Team.Left) {
             if (_gridCombatSystem.leftTeam.Count > 5) {
-                Debug.Log($"{_rightTeam} is FULL");
+                Debug.Log($"{_gridCombatSystem.leftTeam} is FULL");
                 return;
             }
 
             _gridCombatSystem.leftTeam.Add(elementCombatSystem);
             _pickedUnit = element;
             _teamPicking = Team.Right;
-            Debug.Log($"Left picked: {_leftPickedUnit}");
             return;
         }
 
         if ((Team) pickedElement == _teamPicking && _teamPicking == Team.Right) {
             if (_gridCombatSystem.rightTeam.Count > 5) {
-                Debug.Log($"{_rightTeam} is FULL");
+                Debug.Log($"{_gridCombatSystem.rightTeam} is FULL");
                 return;
             }
 
             _gridCombatSystem.rightTeam.Add(elementCombatSystem);
             _pickedUnit = element;
-            Debug.Log($"Right picked: {_rightTeam}");
             _teamPicking = Team.Left;
         }
     }
@@ -88,13 +74,27 @@ public class DraftPickController : MonoBehaviour {
     }
 
     private void Update() {
-        if (Input.GetMouseButtonDown(0) && _pickedUnit != null) {
-            if (_pickedPosition == false) {
-                var pickedUnit = Instantiate(_pickedUnit, CursorUtils.GetMouseWorldPosition(), Quaternion.identity);
-                pickedUnit.transform.localScale = new Vector3(5, 5, 1);
-                pickedUnit.transform.position = GridUtils.SetUnitOnTileCenter(pickedUnit);
-                _pickedUnit = null;
-            }
+        if (!Input.GetMouseButtonDown(0) || _pickedUnit == null) return;
+        Grid<GridCombatSystem.GridObject> grid = GameController_GridCombatSystem.Instance.GetGrid();
+        GridCombatSystem.GridObject gridObject = grid.GetGridObject(CursorUtils.GetMouseWorldPosition());
+        
+        if (gridObject == null) return;
+        var pickedUnit = Instantiate(_pickedUnit, CursorUtils.GetMouseWorldPosition(), Quaternion.identity);
+        pickedUnit.transform.localScale = new Vector3(5, 5, 1);
+        pickedUnit.transform.position = GridUtils.SetUnitOnTileCenter(pickedUnit);
+        _pickedUnit = null;
+
+        if (_teamPicking == Team.Left) {
+         
+            _leftTeamPanel.SetActive(true);
+            _rightTeamPanel.SetActive(false);
+            return;
+        }
+
+        if (_teamPicking == Team.Right) {
+            _leftTeamPanel.SetActive(false);
+            _rightTeamPanel.SetActive(true);
+
         }
     }
 }
